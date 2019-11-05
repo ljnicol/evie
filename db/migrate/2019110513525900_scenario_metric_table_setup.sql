2f768a7f4eb086de5b6e6df288c08eb3f8846cf8
ljnicol <l.nicol.9@gmail.com>
2019-11-05 22:11:24 +1000

    Re-design database structure (simplify)

--- db/migrate/2019110513525900_scenario_metric_table_setup.sql
+++ db/migrate/2019110513525900_scenario_metric_table_setup.sql
@@ -1,41 +1,26 @@
-CREATE TABLE "scenarios"
-(
-    "id"          integer PRIMARY KEY,
-    "name"        varchar UNIQUE NOT NULL,
-    "description" varchar,
-    "assumptions" varchar
+CREATE TABLE "scenarios" (
+  "id" integer PRIMARY KEY,
+  "name" varchar UNIQUE NOT NULL,
+  "description" varchar,
+  "assumptions" varchar
 );
 
-CREATE TABLE "scenario_detail"
-(
-    "id"          integer PRIMARY KEY,
-    "scenario_id" integer NOT NULL,
-    "year"        varchar NOT NULL,
-    unique ("scenario_id", "year")
+CREATE TABLE "metric_data" (
+  "id" integer PRIMARY KEY,
+  "scenario_id" integer NOT NULL,
+  "metric_id" integer NOT NULL,
+  "year" integer NOT NULL,
+  "value" double precision,
+  "spatial_table" varchar,
+  unique ("scenario_id", "metric_id", "year")
 );
 
-CREATE TABLE "metric_data"
-(
-    "id"                   integer PRIMARY KEY,
-    "scenario_detail_id"   integer NOT NULL,
-    "metric_id"            integer NOT NULL,
-    "metric_value"         double precision,
-    "metric_spatial_table" varchar,
-    unique ("scenario_detail_id", "metric_id")
+CREATE TABLE "metrics" (
+  "id" integer PRIMARY KEY,
+  "name" varchar UNIQUE,
+  "description" varchar
 );
 
-CREATE TABLE "metrics"
-(
-    "id"          integer PRIMARY KEY,
-    "name"        varchar UNIQUE,
-    "description" varchar
-);
+ALTER TABLE "metric_data" ADD FOREIGN KEY ("scenario_id") REFERENCES "scenarios" ("id");
 
-ALTER TABLE "scenario_detail"
-    ADD FOREIGN KEY ("scenario_id") REFERENCES "scenarios" ("id");
-
-ALTER TABLE "metric_data"
-    ADD FOREIGN KEY ("scenario_detail_id") REFERENCES "scenario_detail" ("id");
-
-ALTER TABLE "metric_data"
-    ADD FOREIGN KEY ("metric_id") REFERENCES "metrics" ("id");
+ALTER TABLE "metric_data" ADD FOREIGN KEY ("metric_id") REFERENCES "metrics" ("id");
