115aac53c1f964f0863fbf1ac705ec2329bfc30f
ljnicol <ljnicol@users.noreply.github.com>
2020-02-27 15:02:39 +1000

    New structure for lib directory

--- bin/windows/build.ps1
+++ bin/windows/build.ps1
@@ -6,6 +6,4 @@ stack build --copy-bins --local-bin-path release\$tag :evie
 yarn run parcel build assets/elm/index.html -d release\$tag\static --public-url app
 Copy-Item .\assets\config-template.json .\release\$tag\config.json -Recurse
 Copy-Item .\lib\* .\release\$tag -Recurse
-Copy-Item .\assets\static\css .\release\$tag\static\css -Recurse
-Copy-Item .\assets\static\webfonts .\release\$tag\static\webfonts -Recurse
-Copy-Item .\assets\static\lib .\release\$tag\static\lib -Recurse
+Copy-Item .\assets\lib .\release\$tag\lib -Recurse
